156a9fc77936563debf8ef44e242311a051c94b2
Sam Mulube <sam.mulube@cosm.com>
2013-05-31 13:37:54 +0000

    WIP - trying to get tests running again.

--- spec/XivelyJSSpec.js
+++ spec/XivelyJSSpec.js
@@ -1,51 +1,74 @@
 describe("XivelyJS", function() {
-  describe(".feed", function() {
-    describe(".subscribe", function() {
-      beforeEach(function() {
-        this.oldWebSocket = window.WebSocket;
-        delete window.WebSocket;
-        this.oldMozWebSocket = window.MozWebSocket;
-        delete window.MozWebSocket;
-        this.oldSockJS = window.SockJS;
-        delete window.SockJS;
-
-        xively.setKey("my_key");
-        this.mockSocket = {};
-        window.WebSocket = jasmine.createSpy("WebSocket");
-        this.endpoint = "http://example.com";
-        xively.setWSEndpoint(this.endpoint);
-      });
-
-      afterEach(function() {
-        window.WebSocket = this.oldWebSocket;
-        window.MozWebSocket = this.oldMozWebSocket;
-        window.SockJS = this.oldSockJS;
-      });
-
-      it("should connect over websockets", function() {
-        xively.feed.subscribe(123);
-        expect(window.WebSocket).toHaveBeenCalledWith(this.endpoint);
-      });
-
-      it("should prefer MozWebSocket if available", function() {
-        window.MozWebSocket = jasmine.createSpy("MozWebSocket");
-
-        xively.feed.subscribe(123);
-
-        expect(window.WebSocket).not.toHaveBeenCalled();
-        expect(window.MozWebSocket).toHaveBeenCalledWith(this.endpoint);
-      });
-
-      it("should prefer sock.js if available", function() {
-        window.MozWebSocket = jasmine.createSpy("MozWebSocket");
-        window.SockJS = jasmine.createSpy("SockJS")
-
-        xively.feed.subscribe(123);
-
-        expect(window.WebSocket).not.toHaveBeenCalled();
-        expect(window.MozWebSocket).not.toHaveBeenCalled();
-        expect(window.SockJS).toHaveBeenCalledWith(this.endpoint + ':8094');
-      });
-    });
+  beforeEach(function() {
+    // this.xively = new XivelyClient();
   });
+
+  // describe(".setKey", function() {
+    // it("should allow setting the api key", function() {
+      // expect(xively.setKey).toBeDefined();
+    // });
+  // });
+
+  // describe(".VERSION", function() {
+    // it("should expose the version string", function() {
+      // expect(xively.VERSION).toBeDefined();
+    // });
+  // });
+
+  // describe(".setEndpoint", function() {
+    // it("should allow setting the endpoint", function() {
+    // });
+  // });
+
+  // describe(".feed", function() {
+    // describe(".subscribe", function() {
+      // beforeEach(function() {
+        // this.oldWebSocket = window.WebSocket;
+        // delete window.WebSocket;
+        // this.oldMozWebSocket = window.MozWebSocket;
+        // delete window.MozWebSocket;
+        // this.oldSockJS = window.SockJS;
+        // delete window.SockJS;
+
+        // xively.setKey("my_key");
+        // this.mockSocket = {};
+        // window.WebSocket = jasmine.createSpy("WebSocket");
+        // this.endpoint = "http://example.com";
+        // xively.setWSEndpoint(this.endpoint);
+        // xively.setSockJSEndpoint(this.endpoint);
+      // });
+
+      // afterEach(function() {
+        // window.WebSocket = this.oldWebSocket;
+        // window.MozWebSocket = this.oldMozWebSocket;
+        // window.SockJS = this.oldSockJS;
+        // xively.reset();
+      // });
+
+      // it("should connect over websockets", function() {
+        // xively.feed.subscribe(123);
+        // expect(window.WebSocket).toHaveBeenCalledWith(this.endpoint);
+      // });
+
+      // it("should prefer MozWebSocket if available", function() {
+        // window.MozWebSocket = jasmine.createSpy("MozWebSocket");
+
+        // xively.feed.subscribe(123);
+
+        // expect(window.WebSocket).not.toHaveBeenCalled();
+        // expect(window.MozWebSocket).toHaveBeenCalledWith(this.endpoint);
+      // });
+
+      // it("should prefer sock.js if available", function() {
+        // window.MozWebSocket = jasmine.createSpy("MozWebSocket");
+        // window.SockJS = jasmine.createSpy("SockJS")
+
+        // xively.feed.subscribe(123);
+
+        // expect(window.WebSocket).not.toHaveBeenCalled();
+        // expect(window.MozWebSocket).not.toHaveBeenCalled();
+        // expect(window.SockJS).toHaveBeenCalledWith(this.endpoint);
+      // });
+    // });
+  // });
 });
